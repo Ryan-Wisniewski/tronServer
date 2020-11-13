1626a42d800711fb44048ef94d0cda37bb097ce0
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2020-11-13 15:52:37 -0500

    hard bot complete, added check so players cant stack on top of each other

--- assets/hardAi.js
+++ assets/hardAi.js
@@ -2,52 +2,111 @@ backwards = {37:39, 38:40, 39:37, 40:38}
 
 const hardAi = (x, y, direction, coords) => {
     if(direction === null){
-        direction = Math.floor(Math.random() * 4) + 37
-        // direction = 37 //for testin
-        console.log(direction)
+        // direction = Math.floor(Math.random() * 4) + 37
+        direction = 37 //for testin
+        // console.log(direction)
     }
     for(let i = 0; i < coords.length; i++){
         // console.log(coords[i].x,coords[i].y, x,y, direction)
-        
         if(direction === 37 && (coords[i].x === x - 1 || 0 === x - 1) && coords[i].y === y){
-            console.log('changeDirection1', x,y, y- 1)
-            console.log(coords)
-            if(coords.some(coord => (coord.y === y - 1 && coord.x === x || coord.y === y - 2 && coord.x === x))){
-                console.log('go up')
-                 direction = 38
-            } else{
-                console.log('elseeee', coords)
-                 direction = 40
+            console.log('changeDirectionFromLeft',x,y)
+
+            if(coords.some(coord => (
+                0 === y - 1 && coord.x === x
+                || 0 === y - 2 && coord.x === x
+                || coord.y === y - 1 && coord.x === x
+                || coord.y === y - 2 && coord.x === x
+                )))
+            {
+                console.log('go downCHECKKK',x, y, coords[i])
+                direction = 40
+
+            } else if(coords.some(coord => (
+                56 === y + 1 && coord.x === x
+                || 56 === y + 2 && coord.x === x
+                || coord.y === y + 1 && coord.x === x
+                || coord.y === y + 2 && coord.x === x
+                ))){
+                console.log('goUP')
+                direction = 38
+            } else {
+                const randomDir = Math.floor(Math.random() * 2)
+                randomDir === 0 ? direction = 38 : direction = 40
+                console.log('Random choice both dir ok',randomDir, direction)
             }
 
         } else if(direction === 38 && (coords[i].y === y - 1 || 0 === y - 1) && coords[i].x === x){
-            console.log('changeDirection2')
-            if(coords.some(coord => (coord.y === y && coord.x === x - 1 || coord.y === y && coord.x === x- 2))){
-                console.log('go left')
-                 direction = 37
-            } else{
-                console.log('elseeee', coords)
-                 direction = 39
+            console.log('changeDirectionFromUP',x,y)
+
+            if(coords.some(coord => (
+                coord.y === y && 0 === x - 1
+                || coord.y === y && 0 === x - 2
+                || coord.y === y && coord.x === x - 1
+                || coord.y === y && coord.x === x - 2
+                )))
+            {
+                console.log('go Right')
+                direction = 39
+            }  else if(coords.some(coord => (
+                64 === x + 1 && coord.y === y
+                || 64 === x + 2 && coord.y === y
+                || coord.x === x + 1 && coord.y === y
+                || coord.x === x + 2 && coord.y === y
+                ))){
+                console.log('go Left')
+                direction = 37
+            } else {
+                const randomDir = Math.floor(Math.random() * 2)
+                randomDir === 0 ? direction = 37 : direction = 39
             }
 
         } else if(direction === 39 && (coords[i].x === x + 1 || 64 === x + 1) && coords[i].y === y){
-            console.log('changeDirection3')
-            if(coords.some(coord => (coord.y === y + 1 && coord.x === x || coord.y === y + 2 && coord.x === x))){
-                console.log('go up')
-                 direction = 40
-            } else{
-                console.log('elseeee', coords)
-                 direction = 38
+            console.log('changeDirectionFromRight',x,y)
+            if(coords.some(coord => (
+                0 === y - 1 && coord.x === x
+                || 0 === y - 2 && coord.x === x
+                || coord.y === y - 1 && coord.x === x
+                || coord.y === y - 1 && coord.x === x
+                )))
+            {
+                console.log('go down')
+                direction = 40
+            } else if(coords.some(coord => (
+                56 === y + 1 && coord.x === x
+                || 56 === y + 2 && coord.x === x
+                || coord.y === y + 1 && coord.x === x
+                || coord.y === y + 2 && coord.x === x
+                ))){
+                console.log('goUP')
+                direction = 38
+            } else {
+                const randomDir = Math.floor(Math.random() * 2)
+                randomDir === 0 ? direction = 38 : direction = 40
+                console.log('Random choice both dir ok',randomDir, direction)
             }
 
         } else if(direction === 40 && (coords[i].y === y + 1 || 56 === y + 1) && coords[i].x === x){
-            console.log('changeDirection4')
-            if(coords.some(coord => (coord.y === y && coord.x === x + 1 || coord.y === y && coord.x === x + 2))){
-                console.log('go left')
-                 direction = 39
-            } else{
-                console.log('elseeee', coords)
-                 direction = 37
+            console.log('changeDirectionFromDown',x,y)
+            if(coords.some(coord => (
+                coord.y === y && 0 === x - 1
+                || coord.y === y && 0 === x - 2
+                || coord.y === y && coords.x === x - 1
+                || coord.y === y && coords.x === x - 2
+                )))
+                {
+                console.log('go right')
+                direction = 39
+            } else if(coords.some(coord => (
+                64 === x + 1 && coord.y === y
+                || 64 === x + 2 && coord.y === y
+                || coord.x === x + 1 && coord.y === y
+                || coord.x === x + 2 && coord.y === y
+                ))){
+                console.log('go Left')
+                direction = 37
+            } else {
+                const randomDir = Math.floor(Math.random() * 2)
+                randomDir === 0 ? direction = 37 : direction = 39
             }
         }
     }
