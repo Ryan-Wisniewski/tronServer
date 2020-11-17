62e4e6748d3b1f8650d2d51bb073fb1b02ea09eb
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2020-11-17 11:46:27 -0500

    cleaned clg

--- assets/hardAi.js
+++ assets/hardAi.js
@@ -9,7 +9,7 @@ const hardAi = (x, y, direction, coords) => {
     for(let i = 0; i < coords.length; i++){
         // console.log(coords[i].x,coords[i].y, x,y, direction)
         if(direction === 37 && (coords[i].x === x - 1 || 0 === x - 1) && coords[i].y === y){
-            console.log('changeDirectionFromLeft',x,y)
+            // console.log('changeDirectionFromLeft',x,y)
 
             if(coords.some(coord => (
                 0 === y - 1 && coord.x === x
@@ -18,7 +18,7 @@ const hardAi = (x, y, direction, coords) => {
                 || coord.y === y - 2 && coord.x === x
                 )))
             {
-                console.log('go downCHECKKK',x, y, coords[i])
+                // console.log('go downCHECKKK',x, y, coords[i])
                 direction = 40
 
             } else if(coords.some(coord => (
@@ -27,16 +27,16 @@ const hardAi = (x, y, direction, coords) => {
                 || coord.y === y + 1 && coord.x === x
                 || coord.y === y + 2 && coord.x === x
                 ))){
-                console.log('goUP')
+                // console.log('goUP')
                 direction = 38
             } else {
                 const randomDir = Math.floor(Math.random() * 2)
                 randomDir === 0 ? direction = 38 : direction = 40
-                console.log('Random choice both dir ok',randomDir, direction)
+                // console.log('Random choice both dir ok',randomDir, direction)
             }
 
         } else if(direction === 38 && (coords[i].y === y - 1 || 0 === y - 1) && coords[i].x === x){
-            console.log('changeDirectionFromUP',x,y)
+            // console.log('changeDirectionFromUP',x,y)
 
             if(coords.some(coord => (
                 coord.y === y && 0 === x - 1
@@ -45,7 +45,7 @@ const hardAi = (x, y, direction, coords) => {
                 || coord.y === y && coord.x === x - 2
                 )))
             {
-                console.log('go Right')
+                // console.log('go Right')
                 direction = 39
             }  else if(coords.some(coord => (
                 64 === x + 1 && coord.y === y
@@ -53,7 +53,7 @@ const hardAi = (x, y, direction, coords) => {
                 || coord.x === x + 1 && coord.y === y
                 || coord.x === x + 2 && coord.y === y
                 ))){
-                console.log('go Left')
+                // console.log('go Left')
                 direction = 37
             } else {
                 const randomDir = Math.floor(Math.random() * 2)
@@ -61,7 +61,7 @@ const hardAi = (x, y, direction, coords) => {
             }
 
         } else if(direction === 39 && (coords[i].x === x + 1 || 64 === x + 1) && coords[i].y === y){
-            console.log('changeDirectionFromRight',x,y)
+            // console.log('changeDirectionFromRight',x,y)
             if(coords.some(coord => (
                 0 === y - 1 && coord.x === x
                 || 0 === y - 2 && coord.x === x
@@ -69,7 +69,7 @@ const hardAi = (x, y, direction, coords) => {
                 || coord.y === y - 1 && coord.x === x
                 )))
             {
-                console.log('go down')
+                // console.log('go down')
                 direction = 40
             } else if(coords.some(coord => (
                 56 === y + 1 && coord.x === x
@@ -77,16 +77,16 @@ const hardAi = (x, y, direction, coords) => {
                 || coord.y === y + 1 && coord.x === x
                 || coord.y === y + 2 && coord.x === x
                 ))){
-                console.log('goUP')
+                // console.log('goUP')
                 direction = 38
             } else {
                 const randomDir = Math.floor(Math.random() * 2)
                 randomDir === 0 ? direction = 38 : direction = 40
-                console.log('Random choice both dir ok',randomDir, direction)
+                // console.log('Random choice both dir ok',randomDir, direction)
             }
 
         } else if(direction === 40 && (coords[i].y === y + 1 || 56 === y + 1) && coords[i].x === x){
-            console.log('changeDirectionFromDown',x,y)
+            // console.log('changeDirectionFromDown',x,y)
             if(coords.some(coord => (
                 coord.y === y && 0 === x - 1
                 || coord.y === y && 0 === x - 2
@@ -94,7 +94,7 @@ const hardAi = (x, y, direction, coords) => {
                 || coord.y === y && coords.x === x - 2
                 )))
                 {
-                console.log('go right')
+                // console.log('go right')
                 direction = 39
             } else if(coords.some(coord => (
                 64 === x + 1 && coord.y === y
@@ -102,7 +102,7 @@ const hardAi = (x, y, direction, coords) => {
                 || coord.x === x + 1 && coord.y === y
                 || coord.x === x + 2 && coord.y === y
                 ))){
-                console.log('go Left')
+                // console.log('go Left')
                 direction = 37
             } else {
                 const randomDir = Math.floor(Math.random() * 2)
